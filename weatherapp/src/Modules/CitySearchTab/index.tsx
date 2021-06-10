f7640b6adc6847b80952367a77ba272f0c6a0f68
Jakub <jakub.alkhaled@visioncritical.com>
2021-06-11 00:05:35 +0200

    create the Modules folder and start adding files and components into it

---
+++ weatherapp/src/Modules/CitySearchTab/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CitySearchTab = () => {
+  return <h1>root</h1>;
+};
+
+export default CitySearchTab;
---
+++ weatherapp/src/Modules/CityWeatherInfoTab/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CityWeatherInfoTab = () => {
+  return <h1>root</h1>;
+};
+
+export default CityWeatherInfoTab;
