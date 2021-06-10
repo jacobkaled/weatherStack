cf6da7ffa73fcc77390f89bd10a99b3443339eb2
Jakub <jakub.alkhaled@visioncritical.com>
2021-06-11 00:04:15 +0200

    create a theme folder for the app and start adding logic into it

---
+++ weatherapp/src/Theme/index.d.ts
@@ -0,0 +1,10 @@
+export type theme ={
+    sizes:{
+      gutterSmall : string,
+      gutterMedium : string,
+      gutterLarge : string,
+    },
+    colors:{
+      backgroundcolor:string,
+    }
+  }
---
+++ weatherapp/src/Theme/index.ts
@@ -0,0 +1,10 @@
+export const theme ={
+  sizes:{
+    gutterSmall : '2rem',
+    gutterMedium : '4rem',
+    gutterLarge : '6rem',
+  },
+  colors:{
+    backgroundcolor:'lightBlue',
+  }
+}
