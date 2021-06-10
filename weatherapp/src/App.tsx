10ecfc7b70e31289b06bf2b5a7ff10a38030e6f3
jacobkaled <jakub.alkhaled@visioncritical.com>
2021-06-10 16:03:04 +0200

    modify the App component and test the TextInput alongisde the async data fetching

--- weatherapp/src/App.tsx
+++ weatherapp/src/App.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
+import { weatherType } from "./WeatherRestAPI/types";
 import { RequestWeather } from "./WeatherRestAPI/RequestWeather";
+import TextInput from "./components/TextInput";
 
 const App = () => {
-  const x = RequestWeather("www.test.com", "GET", { city: "brazil" });
+  const fetchWeather = async (): Promise<weatherType> => {
+    const res = await RequestWeather("prague");
+    const weatherData: weatherType = await res.json();
+    return weatherData;
+  };
+  useEffect(() => {
+    fetchWeather();
+  }, []);
   return (
     <WeatherWrapper>
-      App test {process.env.REACT_APP_WEATHER_PUBLIC_KEY} <h2>{x}</h2>
+      <TextInput />
     </WeatherWrapper>
   );
 };
